f7357354fca1707962b24731581917d8bc1a4695
diamondkiss <simeonugochukwu.su@gmail.com>
2023-02-16 15:42:05 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - print the code using puts
+ *
+ * Return: 0 if success
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
