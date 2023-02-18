7a47908da771155f4e83fc98e4ba7471d4fe8b15
diamondkiss <simeonugochukwu.su@gmail.com>
2023-02-17 20:25:17 -0800

    4-print_alphabt

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,13 +10,14 @@ int main(void)
 {
 	char a;
 
-	/* print lowercase letters */
 	for (a = 'a'; a <= 'Z'; a++)
 	{
 		if (a == 'q' || a == 'e')
 			continue;
-		putchar(a);
+
+		/**putchar(a);*/
 	}
+	putchar(a);
 	putchar('\n');
 	return (0);
 }
