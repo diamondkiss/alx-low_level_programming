78472a89fb5f3a599a2926c81740f9ca7babf7bc
diamondkiss <simeonugochukwu.su@gmail.com>
2023-02-19 13:14:39 -0800

    100-print_comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * Return: Always 0 (success)
+ **/
+
+int main(void)
+{
+	int a, b;
+
+	for (a = 0; a < 8; a++)
+{
+	for (b = a + 1; b < 9; b++)
+	{
+		if (a != b)
+		{
+			putchar(a + '0');
+			putchar(b + '0');
+			if (a + b < 17)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+}
+	putchar('\n');
+	return (0);
+}
