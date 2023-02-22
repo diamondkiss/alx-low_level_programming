a4f199a7644f7a733d892eab4cd09524825db43c
diamondkiss <simeonugochukwu.su@gmail.com>
2023-02-22 13:24:00 -0800

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Write a program that prints _putchar, followed by a new line
+ *
+ *
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = _putcha;)
+	{
+		putchar(a)
+	}
+	putchar('\n');
+	return (0);
+}
