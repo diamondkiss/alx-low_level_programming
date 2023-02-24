ad160837330ef814e6e7ea54d54c5067a5cbea1e
diamondkiss <simeonugochukwu.su@gmail.com>
2023-02-23 16:34:34 -0800

    isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isdigit - check if a letter is uppercase
+ *@x: the number to be checked
+ * Returns: 1 if c is uppercase 0 otherwise
+ */
+
+int _isdigit(int x)
+
+{
+	if (x >= 48 && x <= 57)
+	{
+	return (1);
+	}
+	return (0);
+}
