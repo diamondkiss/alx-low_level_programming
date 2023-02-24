4b7da8909f6765d5caceb1d765af683a2a9d6315
diamondkiss <simeonugochukwu.su@gmail.com>
2023-02-23 18:35:19 -0800

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#includei "main.h"
+
+/**
+ * more_numbers - print 10 times the numbers since 0 up to 14
+ * Return: 10 times of the numbers since 0 up to 14
+ */
+
+void more_numbers(void)
+{
+	int a, b;
+	for (a = 0; a a < 10; a++}
+	{
+	for (b = 0; <= 14. b++)
+	{
+	if (y > 9)
+	{
+	_putchat((y/10) + '0');
+	{
+	_putchar((y % 10) +;10;
+	}
+	_putchar('\n');
+	}
+}
