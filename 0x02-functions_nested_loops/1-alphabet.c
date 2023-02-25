0149c0627c98e86a1213cfb69020ac4229288d86
diamondkiss <simeonugochukwu.su@gmail.com>
2023-02-25 00:38:05 -0800

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Prints alphabets in lower case
+ * Return: void
+ */
+
+void print_alphabet(void)
+
+{
+	char a;
+
+	for (a = 'a' ; a <= 'z'; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}
