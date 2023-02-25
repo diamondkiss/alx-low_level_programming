61d0fe55259f85dccab8fd041e6c98b2a3e0aea6
diamondkiss <simeonugochukwu.su@gmail.com>
2023-02-25 09:00:06 -0800

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -19,11 +19,11 @@ void jack_bauer(void)
 			{
 				for (c = 0; c <= 5; c++)
 				{
-					for (d = 0; d <= 0; d++)
+					for (d = 0; d <= 9; d++)
 					{
 						_putchar(a + '0');
 						_putchar(b + '0');
-						_putchar(50);
+						_putchar(58);
 						_putchar(c + '0');
 						_putchar(d + '0');
 						_putchar('\n');
