d4cf4aae93ae8d4371dc8a2418d8609eeaafd5f2
diamondkiss <simeonugochukwu.su@gmail.com>
2023-02-26 13:19:21 -0800

    100-times_table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -20,7 +20,7 @@ void print_times_table(int n)
 			for (mult = 1; mult <= n; mult++)
 			{
 				_putchar(',');
-				_putchar(',');
+				_putchar(' ');
 				prod = num * mult;
 				if (prod <= 99)
 					_putchar(' ');
