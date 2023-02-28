6a9533e7380c44c55620c5ad04d3e74d8bb7dcfa
diamondkiss <simeonugochukwu.su@gmail.com>
2023-02-28 14:38:28 -0800

    3-puts

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * _puts - Prints a string
@@ -11,7 +12,7 @@ void _puts(char *str)
 {
 	for (; *str != '\0'; str++)
 	{
-	_putchar(*str);
+	putchar(*str);
 	}
-	_putchar('\n');
+	putchar('\n');
 }
