3df759e6e2ce8969f017aa1ef147fd3e8bc83896
diamondkiss <simeonugochukwu.su@gmail.com>
2023-03-03 14:58:35 -0800

    5-string_toupper

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -2,16 +2,16 @@
 
 /**
  * string_toupper - Changes all lowercase letters
- * of a string to uppercase
- * @str: The string to be changed
- * Return: A pointer to the changed string
+ * of a string to uppercase.
+ * @str: The string to be changed.
+ * Return: A pointer to the changed string.
  *
  */
 
 char *string_toupper(char *str)
 
 {
-	
+
 	int index = 0;
 
 	while (str[index])
