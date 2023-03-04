23afa5148b967b741ae90a5651df1218df8fd7fd
diamondkiss <simeonugochukwu.su@gmail.com>
2023-03-04 10:32:54 -0800

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -7,7 +7,7 @@
  * Return: *s
  */
 
-char *rot13(char *)
+char *rot13(char *s)
 {
 	int i;
 	int j;
@@ -15,6 +15,7 @@ char *rot13(char *)
 "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 	char datarot[] = 
 "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; j < 52; j++)
