b133a45bfb29721ac1940c1dc699c7ad18b19fe7
diamondkiss <simeonugochukwu.su@gmail.com>
2023-03-10 03:24:01 -0800

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -20,7 +20,7 @@ int _strlen_recursion(char *s)
  * @n2: biggest iterator
  * Return:.
  */
-int compartor(char *s, int n1, int n2)
+int comparator(char *s, int n1, int n2)
 {
 	if (*(s + n1) == *(s + n2))
 	{
